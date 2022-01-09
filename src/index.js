e768c88a43e69b5d60f4e2a065e6c8831aaef80d
sam <swilcox21@gmail.com>
2022-01-09 17:00:59 -0500

    finished setting up the boiler plate for all future projects

--- src/index.js
+++ src/index.js
@@ -12,10 +12,6 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
-store.subscribe(() => {
-  console.log('state changed' + store.getState());
-});
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
